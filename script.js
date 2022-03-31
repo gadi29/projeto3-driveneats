4f6152981fbcf60f6b1594fe803ab78ce1457033
Gadiel <gad.azevedo@gmail.com>
2022-03-31 12:05:36 -0300

    terceira versão

---
+++ script.js
@@ -0,0 +1,53 @@
+function escolherFrango() {
+    document.getElementById("frango").style.borderColor = "#32B72F";
+    document.getElementById("carbonara").style.borderColor = "#FFFFFF";
+    document.getElementById("feijoada").style.borderColor = "#FFFFFF";
+}
+
+function escolherCarbonara() {
+    document.getElementById("carbonara").style.borderColor = "#32B72F";
+    document.getElementById("frango").style.borderColor = "#FFFFFF";
+    document.getElementById("feijoada").style.borderColor = "#FFFFFF";
+}
+
+function escolherFeijoada() {
+    document.getElementById("feijoada").style.borderColor = "#32B72F";
+    document.getElementById("carbonara").style.borderColor = "#FFFFFF";
+    document.getElementById("frango").style.borderColor = "#FFFFFF";
+}
+
+function escolherSuco() {
+    document.getElementById("suco").style.borderColor = "#32B72F";
+    document.getElementById("pureza").style.borderColor = "#FFFFFF";
+    document.getElementById("chocolate").style.borderColor = "#FFFFFF";
+}
+
+function escolherPureza() {
+    document.getElementById("pureza").style.borderColor = "#32B72F";
+    document.getElementById("suco").style.borderColor = "#FFFFFF";
+    document.getElementById("chocolate").style.borderColor = "#FFFFFF";
+}
+
+function escolherChocolate() {
+    document.getElementById("chocolate").style.borderColor = "#32B72F";
+    document.getElementById("pureza").style.borderColor = "#FFFFFF";
+    document.getElementById("suco").style.borderColor = "#FFFFFF";
+}
+
+function escolherPave() {
+    document.getElementById("pave").style.borderColor = "#32B72F";
+    document.getElementById("pudim").style.borderColor = "#FFFFFF";
+    document.getElementById("gelatina").style.borderColor = "#FFFFFF";
+}
+
+function escolherPudim() {
+    document.getElementById("pudim").style.borderColor = "#32B72F";
+    document.getElementById("pave").style.borderColor = "#FFFFFF";
+    document.getElementById("gelatina").style.borderColor = "#FFFFFF";
+}
+
+function escolherGelatina() {
+    document.getElementById("gelatina").style.borderColor = "#32B72F";
+    document.getElementById("pudim").style.borderColor = "#FFFFFF";
+    document.getElementById("pave").style.borderColor = "#FFFFFF";
+}
