c93e226440ee01fa1cb918dd0988cd6de387c9ca
Gadiel <gad.azevedo@gmail.com>
2022-04-01 22:52:18 -0300

    quarta versão

--- script.js
+++ script.js
@@ -1,53 +1,29 @@
-function escolherFrango() {
-    document.getElementById("frango").style.borderColor = "#32B72F";
-    document.getElementById("carbonara").style.borderColor = "#FFFFFF";
-    document.getElementById("feijoada").style.borderColor = "#FFFFFF";
+function escolherPrato(prato) {
+    const botaoSelecionado = document.querySelector(".prato-principal").querySelector(".selecionado");
+    
+    if (botaoSelecionado !== null) {
+        botaoSelecionado.classList.remove("selecionado");
+    }
+    
+    prato.classList.add("selecionado");
 }
 
-function escolherCarbonara() {
-    document.getElementById("carbonara").style.borderColor = "#32B72F";
-    document.getElementById("frango").style.borderColor = "#FFFFFF";
-    document.getElementById("feijoada").style.borderColor = "#FFFFFF";
+function escolherBebida(bebida) {
+    const botaoSelecionado = document.querySelector(".bebida").querySelector(".selecionado");
+    
+    if (botaoSelecionado !== null) {
+        botaoSelecionado.classList.remove("selecionado");
+    }
+    
+    bebida.classList.add("selecionado");
 }
 
-function escolherFeijoada() {
-    document.getElementById("feijoada").style.borderColor = "#32B72F";
-    document.getElementById("carbonara").style.borderColor = "#FFFFFF";
-    document.getElementById("frango").style.borderColor = "#FFFFFF";
-}
-
-function escolherSuco() {
-    document.getElementById("suco").style.borderColor = "#32B72F";
-    document.getElementById("pureza").style.borderColor = "#FFFFFF";
-    document.getElementById("chocolate").style.borderColor = "#FFFFFF";
-}
-
-function escolherPureza() {
-    document.getElementById("pureza").style.borderColor = "#32B72F";
-    document.getElementById("suco").style.borderColor = "#FFFFFF";
-    document.getElementById("chocolate").style.borderColor = "#FFFFFF";
-}
-
-function escolherChocolate() {
-    document.getElementById("chocolate").style.borderColor = "#32B72F";
-    document.getElementById("pureza").style.borderColor = "#FFFFFF";
-    document.getElementById("suco").style.borderColor = "#FFFFFF";
-}
-
-function escolherPave() {
-    document.getElementById("pave").style.borderColor = "#32B72F";
-    document.getElementById("pudim").style.borderColor = "#FFFFFF";
-    document.getElementById("gelatina").style.borderColor = "#FFFFFF";
-}
-
-function escolherPudim() {
-    document.getElementById("pudim").style.borderColor = "#32B72F";
-    document.getElementById("pave").style.borderColor = "#FFFFFF";
-    document.getElementById("gelatina").style.borderColor = "#FFFFFF";
-}
-
-function escolherGelatina() {
-    document.getElementById("gelatina").style.borderColor = "#32B72F";
-    document.getElementById("pudim").style.borderColor = "#FFFFFF";
-    document.getElementById("pave").style.borderColor = "#FFFFFF";
+function escolherSobremesa(sobremesa) {
+    const botaoSelecionado = document.querySelector(".sobremesa").querySelector(".selecionado");
+    
+    if (botaoSelecionado !== null) {
+        botaoSelecionado.classList.remove("selecionado");
+    }
+    
+    sobremesa.classList.add("selecionado");
 }
